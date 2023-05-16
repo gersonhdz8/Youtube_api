9352fdd8dec2dd25253bd9b6138c51cbb2dacba8
GersonHdz <gersonhdz8@gmail.com>
2023-05-16 03:44:41 -0500

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "gothic":['Pathway Gothic One', 'sans-serif'],    
+
+      },
+    },
+  plugins: [],
+}
+}
+
